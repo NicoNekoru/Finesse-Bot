3ce15016b26f602f5d0b6f35f13e79717763dbb0
Neko <66227158+NicoNekoru@users.noreply.github.com>
2021-05-05 19:53:14 -0600

    fixed bug

--- main.js
+++ main.js
@@ -5,33 +5,33 @@ const Client = new Discord.Client()
 const CClient = Challonge.createClient(CONFIG.challonge)
 Client.login(CONFIG.discord.token)
 require(`./eventloader.js`)(Client, CClient)
-client.commands = new Map();
-client.aliases = new Map();
-client.queue = new Map();
+Client.commands = new Map();
+Client.aliases = new Map();
+Client.queue = new Map();
 fs.readdir('./commands/', (err, files) => {
 	if (err) console.error(err);
 	files.forEach(f => {
 		let props = require(`./commands/${f}`);
-		client.commands.set(props.help.name, props);
+		Client.commands.set(props.help.name, props);
 		props.help.aliases.forEach(alias => {
-			client.aliases.set(alias, props.help.name);
+			Client.aliases.set(alias, props.help.name);
 		});
 		console.log(`${props.help.name} loaded at ${new Date()}`)
 	});
 });
 
-client.reload = (client, command) => {
+Client.reload = (Client, command) => {
 	return new Promise((resolve, reject) => {
 		try {
 			delete require.cache[require.resolve(`./commands/${command}`)];
 			let cmd = require(`./commands/${command}`);
-			client.commands.delete(command);
-			client.aliases.forEach((cmd, alias) => {
-				if (cmd === command) client.aliases.delete(alias);
+			Client.commands.delete(command);
+			Client.aliases.forEach((cmd, alias) => {
+				if (cmd === command) Client.aliases.delete(alias);
 			});
-			client.commands.set(command, cmd);
+			Client.commands.set(command, cmd);
 			cmd.help.aliases.forEach(alias => {
-				client.aliases.set(alias, cmd.help.name);
+				Client.aliases.set(alias, cmd.help.name);
 			});
 			resolve();
 		} catch (e) {
